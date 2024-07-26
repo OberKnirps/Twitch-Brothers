f2efd0cadacc30b5edb90dfa7a58a7605bb7b79e
OberKnirps <oberknirps@vivaldi.net>
2024-07-26 14:27:16 +0200

    integrate hard coded settings to tweak event votes and link to ingame mod settings later

--- ui/mods/mod_twitch_brothers/twitch_world_event_screen.js
+++ ui/mods/mod_twitch_brothers/twitch_world_event_screen.js
@@ -13,10 +13,44 @@ var TwitchEventVotes = function()
     this.decisions = [];
     this.voterList = {};
     this.voteRunning = false;
+    this.gotFirstVote = false;
 
     this.timerCount = 0;
     this.timer = null;
     this.timeout = null;
+    this.startTime = null;
+
+    this.settings = 
+    {
+        active: true,
+        timer:
+        {
+            active: true,
+            waitForVote: true,
+            duration: 5, //in sec
+            selection: 
+            {
+                manual: true,
+                auto: true,
+                duration: 1, //in sec
+                speed: 10, //1-100
+                delay: 2, //in sec
+            },
+            randomizeChoice:
+            {
+                active: true,
+                duration: 1, //in sec
+                speed: 10 //1-100
+            }        
+        },
+        color:
+        {
+            active: true,
+            colorGood: '#00ff00',
+            colorBad: '#ff0000',
+            voteOffset: 2
+        }
+    }
 
     var self = this;
 
@@ -28,11 +62,20 @@ var TwitchEventVotes = function()
         this.mContainer.append(self.timer);
     }
 
+    var oldFunc2 = WorldEventScreen.prototype.hide;
+    WorldEventScreen.prototype.hide = function (_eventData, _withSlideAnimation)
+    {
+        clearTimeout(self.timeout); //make sure events aren't triggered after a decision is manually selected
+        oldFunc2.apply(this,[_eventData, _withSlideAnimation]);
+    }
 
     var oldFunc = WorldEventScreen.prototype.renderButtons;
     WorldEventScreen.prototype.renderButtons = function (_data, _hasBigButtons)
     {
+        clearTimeout(self.timeout); //make sure events aren't triggered after a decision is manually selected
         oldFunc.apply(this,[_data, _hasBigButtons]);
+        if(!self.settings.active) return;
+
         var rows = this.mButtonContainer.children();
         
         self.newEventVote();
@@ -49,82 +92,108 @@ var TwitchEventVotes = function()
             self.decisions[i] = new TwitchEventDecision(currentRow.find('.ui-control').first(), currentRow.find('span'), eventCounter);
         }
 
-        self.timerCount = 10;
-        self.timer.text(self.timerCount + 's');
-        
-        if(self.timeout != null)
-            clearTimeout(self.timeout);
-        self.timeout = setTimeout(function eventSecond()
+        self.timer.text('');
+        if(self.settings.timer.active)
         {
-            self.timerCount--;
+            self.timerCount = self.settings.timer.duration;
             self.timer.text(self.timerCount + 's');
-
-            if(self.timerCount != 0)
+            
+            if(self.timeout != null)
+                clearTimeout(self.timeout);
+            self.timeout = setTimeout(function eventSecond()
             {
-                self.timeout = setTimeout(eventSecond, 1000);
-            }else{
-                /*trigger event decision*/
-                self.stopVote();
-
-                //get most voted decision
-                var max = [];
-                max.push(self.decisions[0]);
-                for (var i = 1; i < self.decisions.length; i++) 
+                if(!self.gotFirstVote && self.settings.timer.waitForVote)
                 {
-                    if (max[0].count <= self.decisions[i].count) 
-                    {
-                        if (max[0].count < self.decisions[i].count)
-                            max = [];
-                        max.push(self.decisions[i]);
-                    }
+                    self.timeout = setTimeout(eventSecond, 1000);
+                    return;
                 }
+                self.timerCount--;
+                self.timer.text(self.timerCount + 's');
 
-                //select decision //TODO make decisions not interactable
-                var selected = Math.floor(Math.random() * max.length);
-                
-                //show selected decision
-                function triggerEvent()
+                if(self.timerCount != 0)
                 {
-                    max[selected].button.click();
-                }
-
-                if(max.length == 1)
-                {
-                    var i = 0;
-                    setTimeout(function blink()
-                        {
-                            if(i % 2 == 0)
-                                max[0].button.addClass('is-selected')
-                            else
-                                max[0].button.removeClass('is-selected')
-
-                            if(i < 6)
-                                setTimeout(blink,300);
-                            else
-                                setTimeout(triggerEvent,1000);
-                            i++;
-                        }, 0);
+                    self.timeout = setTimeout(eventSecond, 1000);
                 }
                 else
                 {
-                    var blinkIntervall = 1;
-                    var minBlinkCount = 45;
-                    var curr = 0;
-                    var prev = 0;
-                    setTimeout(function blink()
+                    /*trigger event decision*/
+                    self.stopVote();
+
+                    //get most voted decision
+                    var max = [];
+                    max.push(self.decisions[0]);
+                    for (var i = 1; i < self.decisions.length; i++) 
+                    {
+                        if (max[0].count <= self.decisions[i].count) 
                         {
-                            max[prev % max.length].button.removeClass('is-selected');
-                            max[curr % max.length].button.addClass('is-selected');
-                            if (curr < minBlinkCount || curr % max.length != selected)
-                                setTimeout(blink,blinkIntervall * curr);
-                            else
-                                setTimeout(triggerEvent,1000);
-                            prev = curr;
-                            curr++;
-                        }, 0);
-                }
-            }    
-        },1000);
+                            if (max[0].count < self.decisions[i].count)
+                                max = [];
+                            max.push(self.decisions[i]);
+                        }
+                    }
+
+                    //select decision //TODO make decisions not interactable (when the selection delay starts?)
+                    var selected = Math.floor(Math.random() * max.length);
+                    
+                    //show selected decision
+                    function triggerEvent()
+                    {
+                        if(self.settings.timer.selection.auto)
+                            max[selected].button.click();
+                    }
+
+                    var selectionBlink = (function ()
+                        {
+                            var i = 0; //static because of self-invoking function
+                            return function ()
+                            {
+                                if(i % 2 == 0)
+                                    max[selected].button.addClass('is-selected')
+                                else
+                                    max[selected].button.removeClass('is-selected')
+
+                                if(Date.now() - self.startTime.getTime() < self.settings.timer.selection.duration * 1000
+                                    || i % 2 == 1)
+                                    setTimeout(selectionBlink, 600/self.settings.timer.selection.speed);
+                                else
+                                    setTimeout(triggerEvent, self.settings.timer.selection.delay * 1000);
+                                i++;
+                            }
+                        })();
+
+                    var blink = (function () 
+                        {
+                            var blinkIntervall = 100/self.settings.timer.randomizeChoice.speed;
+                            var curr = 0;
+                            var prev = 0;
+
+                            return function ()
+                            {
+                                max[prev % max.length].button.removeClass('is-selected');
+                                max[curr % max.length].button.addClass('is-selected');
+                                if (Date.now() - self.startTime.getTime() < self.settings.timer.randomizeChoice.duration * 1000 || curr % max.length != selected)
+                                    setTimeout(blink,blinkIntervall * curr);
+                                else{
+                                    self.startTime = new Date();
+                                    setTimeout(selectionBlink, 0);
+                                }
+                                prev = curr;
+                                curr++;  
+                            }
+                        })();
+
+                    self.startTime = new Date();
+                    if(max.length == 1 || !self.timer.randomizeChoice.active)
+                    {
+                        setTimeout(selectionBlink, 0);
+                    }
+                    else
+                    {
+                        setTimeout(blink, 0);
+                    }
+                }    
+            },1000);
+        }
     }
 };
 
@@ -135,6 +204,7 @@ TwitchEventVotes.prototype.voteForEvent = function (_eventId, _value)
         this.decisions[_eventId].count += _value;
         this.decisions[_eventId].counter.text(this.decisions[_eventId].count);
         this.updateEventColors();
+        this.gotFirstVote = true;
     }
 }
 
@@ -154,28 +224,32 @@ TwitchEventVotes.prototype.newEventVote = function ()
     this.decisions = [];
     this.voterList = {};
     this.voteRunning = true;
+    this.gotFirstVote = false;
 }
 
 TwitchEventVotes.prototype.stopVote = function ()
 {
     this.voteRunning = false;
+    this.gotFirstVote = false;
 }
 
 TwitchEventVotes.prototype.updateEventColors = function ()
 {
-    var offset = 3;
+    if (!this.settings.color.active) 
+        return;
+    var offset = this.settings.color.voteOffset;
     var sum = 0;
     for(var i = 0; this.decisions.length > i; i++)
         sum += this.decisions[i].count + offset;
-    var avrg = sum/(this.decisions.length-1);
+    var avrg = sum/this.decisions.length;
 
     for(var i = 0; this.decisions.length > i; i++)
     {
         if(this.decisions[i].count + offset < avrg)
         {
-            this.decisions[i].label.css("color", interpolateColors("#ff0000", "#dda21f", (this.decisions[i].count + offset) / avrg));
+            this.decisions[i].label.css("color", interpolateColors(this.settings.color.colorBad, "#dda21f", (this.decisions[i].count + offset) / avrg));
         }else{
-            this.decisions[i].label.css("color", interpolateColors("#dda21f", "#00ff00", (this.decisions[i].count + offset - avrg) / (sum - avrg)));
+            this.decisions[i].label.css("color", interpolateColors("#dda21f", this.settings.color.colorGood, (this.decisions[i].count + offset - avrg) / (sum - avrg)));
         }
     }
 }
